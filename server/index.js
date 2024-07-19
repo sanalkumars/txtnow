f3e15c1757c8bf7e1bac584fa6415899666ab292
Gibi paul <info@nubicus.com>
2024-07-19 18:19:39 +0530

    server basic setup finished

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 import express from 'express'
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 const app = express();
-console.log("server")
+
+const port= process.env.PORT || 3000;
+const mongoUrl = process.env.MONGO_URL;
+
+ const server = app.listen(port,()=>{
+    console.log(`server running at http://localhost:{port}`);
+ });
+
+mongoose.connect(mongoUrl)
+.then(()=>{
+    console.log("database connected successfully");
+}).catch(err=>console.log(err));
+
